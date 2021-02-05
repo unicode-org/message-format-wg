8fe67840d31187fb2fa14e3009f3eb6cd6444181
Mihai Nita <nmihai_2000@yahoo.com>
2021-02-04 18:27:04 -0800

    Implemented SelectorMessage

--- experiments/data_model/ts_mihai/src/messageformat.ts
+++ experiments/data_model/ts_mihai/src/messageformat.ts
@@ -7,6 +7,115 @@ import {ISelectorMessage, } from './imessageformat';
 import {ISimpleMessage} from './imessageformat';
 import {ISwitch} from './imessageformat';
 
+function mapToObject<T>(obj?: Map<string, T>) : {[k: string]: T} {
+	const result: {[k: string]: T} = {};
+	if (obj) {
+		obj.forEach((val: T, key: string) => {
+			result[key] = val;
+		});
+	}
+	return result;
+}
+
+interface ISwitchSelectorFunction {
+	// (name: string, type: string, flags: Map<string, string>,
+	// 	locale: string, parameters: Map<string, unknown>): ICase;
+	(value1: unknown, value2: unknown, locale: string): number;
+}
+interface IFormatterFunction {
+	(name: string, type: string, flags: Map<string, string>,
+		locale: string, parameters: Map<string, unknown>): string;
+}
+
+const formatDateTime: IFormatterFunction = (
+		name: string,
+		type: string,
+		flags: Map<string, string>,
+		locale: string,
+		parameters: Map<string, unknown>) => {
+
+	const options = mapToObject<string>(flags);
+	if (type == 'date' || type == 'time') {
+		const value = parameters.get(name);
+		if (value instanceof Date) {
+			return Intl.DateTimeFormat(locale, options).format(value);
+		}
+		if (value instanceof Number) {
+			return Intl.DateTimeFormat(locale, options).format(value.valueOf());
+		}
+	}
+	return '<undefined ' + name + '>';
+};
+
+const formatNumber: IFormatterFunction = (
+		name: string,
+		type: string,
+		flags: Map<string, string>,
+		locale: string,
+		parameters: Map<string, unknown>) => {
+
+	const options = mapToObject<string>(flags);
+	if (type == 'number') {
+		const value = parameters.get(name);
+		if (value instanceof Number || typeof value === 'number') {
+			return Intl.NumberFormat(locale, options).format(value.valueOf());
+		}
+	}
+	return '<undefined ' + name + '>';
+};
+
+const _defaultFormatterFunctions = new Map<string, IFormatterFunction>([
+	['date', formatDateTime],
+	['time', formatDateTime],
+	['number', formatNumber]
+]);
+
+const pluralSwitchSelector: ISwitchSelectorFunction = (
+		value1: unknown, value2: unknown, locale: string) => {
+	let value1Str = '' + value1;
+	let value2Str = '' + value2;
+	if (value1Str == value2Str) {
+		return 10;
+	}
+	if (value1 instanceof Number || typeof value1 === 'number') {
+		let sel = new Intl.PluralRules(locale).select(value1.valueOf());
+		if (sel == value2Str) {
+			return 5;
+		}
+	}
+	if (value2Str == 'other')
+		return 2;
+	return -100000;
+};
+
+const genderSwitchSelector: ISwitchSelectorFunction = (
+		value1: unknown, value2: unknown, locale: string) => {
+	// the gender selector is just syntactic sugar, for now
+	return selectSwitchSelector(value1, value2, locale);
+};
+
+const selectSwitchSelector: ISwitchSelectorFunction = (
+		value1: unknown, value2: unknown, locale: string) => {
+	if (value1 == value2) {
+		return 10;
+	}
+	let value1Str = '' + value1;
+	let value2Str = '' + value2;
+	if (value1Str == value2Str) {
+		return 5;
+	}
+	if (value2Str == 'other') {
+		return 2;
+	}
+	return -100000;
+};
+
+const _defaultSwitchSelectorFunctions = new Map<string, ISwitchSelectorFunction>([
+	['plural', pluralSwitchSelector],
+	['gender', genderSwitchSelector],
+	['select', selectSwitchSelector]
+]);
+
 export abstract class Message implements IMessage {
 	id: string;
 	locale: string;
@@ -43,11 +152,45 @@ export class SelectorMessage extends Message implements ISelectorMessage {
 	messages: Map<ICase[], ISimpleMessage>;
 	constructor(id: string, locale: string, switches: Switch[], messages: Map<ICase[], ISimpleMessage>) {
 		super(id, locale);
+		// Need way better validation that this for prod (types, null, etc.)
+		messages.forEach((value: ISimpleMessage, key: ICase[]) => {
+			if (switches.length != key.length) {
+				throw new Error('Switch count different than case count:\n'
+					+ switches.length
+					+ ' != '
+					+ key.length
+				);
+			}
+		});
 		this.switches = switches;
 		this.messages = messages;
 	}
 	format(parameters: Map<string, unknown>): string {
-		throw new Error('Method not implemented yet.\nParameters: ' + parameters);
+		let bestScore = -1;
+		var bestMessage = new SimpleMessage(this.id, this.locale, []);
+		this.messages.forEach((msgVal: SimpleMessage, caseElement: ICase[]) => {
+			let currentScore = -1;
+			for (var i = 0; i < this.switches.length; i++) {
+				const switchElement = this.switches[i];
+				const value = parameters.get(switchElement.name);
+				const switchFunction = _defaultSwitchSelectorFunctions.get(switchElement.type);
+				if (switchFunction) {
+					const score = switchFunction(value, caseElement[i], this.locale);
+					currentScore += score;
+				}
+			}
+			if (currentScore > bestScore) {
+				bestScore = currentScore;
+				bestMessage = msgVal;
+			}
+		});
+		if (bestScore >= 1) {
+			return bestMessage.format(parameters);
+		}
+		console.log("=== ERROR ===");
+		console.log(this);
+		console.log(parameters);
+		throw new Error('Some troubles.\nParameters: ' + parameters);
 	}
 }
 
@@ -75,12 +218,6 @@ export class Placeholder implements IPlaceholder {
 	type: string;
 	flags: Map<string, string>;
 
-	// _default_known_formatters = new Map<string, IFormatter>([
-	// 	['date', Intl.DateTimeFormat],
-	// 	['time', Intl.DateTimeFormat],
-	// 	['number', Intl.NumberFormat]
-	// ]);
-
 	constructor(name: string, type: string, flags: Map<string, string>) {
 		this.name = name;
 		this.type = type;
@@ -88,28 +225,9 @@ export class Placeholder implements IPlaceholder {
 	}
 	format(locale: string, parameters: Map<string, unknown>): string {
 		const value = parameters.get(this.name); // the runtime value of the placeholder
-
-		const options: {[k: string]: unknown} = {};
-		this.flags.forEach((val: string, key: string) => {
-			options[key] = val;
-		});
-
-		// Need to figure out how to create a map of formatters.
-		// Did in in JavaScript, have to figure out how to do in in TypeScript.
-		// So that we can do something like this:
-		//     fmt = this._default_known_formatters.get(this.type);
-		// and support custom formatter types.
-		if (this.type == 'date' || this.type == 'time') {
-			if (value instanceof Date) {
-				return Intl.DateTimeFormat(locale, options).format(value);
-			}
-			if (value instanceof Number) {
-				return Intl.DateTimeFormat(locale, options).format(value.valueOf());
-			}
-		} else if (this.type == 'number') {
-			if (value instanceof Number || typeof value === "number") {
-				return Intl.NumberFormat(locale, options).format(value.valueOf());
-			}
+		const formatterFunction = _defaultFormatterFunctions.get(this.type);
+		if (formatterFunction) {
+			return formatterFunction(this.name, this.type, this.flags, locale, parameters);
 		} else if (value) {
 			return String(value);
 		}
--- experiments/data_model/ts_mihai/test/messageformat.test.ts
+++ experiments/data_model/ts_mihai/test/messageformat.test.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import 'mocha';
-import { PlainText, Placeholder, SimpleMessage } from '../src/messageformat';
+import { PlainText, Placeholder, SimpleMessage, SelectorMessage, Switch } from '../src/messageformat';
+import { ICase, ISimpleMessage } from '../src/imessageformat';
 
 describe('Tests for MessageFormat:', () => {
 
 	// Helper functions to reduce verbosity
-
 	function objectToMap<T>(obj?: {[k: string]: T}) : Map<string, T> {
 		const result = new Map<string, T>();
 		for (const key in obj) {
@@ -14,7 +14,7 @@ describe('Tests for MessageFormat:', () => {
 		return result;
 	}
 
-	// Common for all tests
+	// Common for all tests. Chosen for the interesting number format.
 	const locale = 'en-IN';
 
 	// The tests
@@ -28,7 +28,7 @@ describe('Tests for MessageFormat:', () => {
 		];
 
 		// TODO: locale should be passed to the constructor, not to format(...)
-		const mf = new SimpleMessage("id", locale, parts);
+		const mf = new SimpleMessage('id', locale, parts);
 		const msgArgs = objectToMap<unknown>({ user: 'John' });
 		// Also a friendliner method, something that takes a JS `unknown`, not a Map
 		const actual = mf.format(msgArgs);
@@ -46,7 +46,7 @@ describe('Tests for MessageFormat:', () => {
 			new PlainText('.\n')
 		];
 
-		const mf = new SimpleMessage("id", locale, parts);
+		const mf = new SimpleMessage('id', locale, parts);
 		const msgArgs = objectToMap<unknown>({ locale: locale, theDay: new Date(2019, 11, 29) });
 		const actual = mf.format(msgArgs);
 
@@ -61,7 +61,7 @@ describe('Tests for MessageFormat:', () => {
 			new PlainText('\n')
 		];
 
-		const mf = new SimpleMessage("id", locale, parts);
+		const mf = new SimpleMessage('id', locale, parts);
 		const msgArgs = objectToMap<unknown>({ bigCount: 1234567890.97531 });
 		const actual = mf.format(msgArgs);
 
@@ -76,10 +76,126 @@ describe('Tests for MessageFormat:', () => {
 			new PlainText('.\n')
 		];
 
-		const mf = new SimpleMessage("id", locale, parts);
+		const mf = new SimpleMessage('id', locale, parts);
 		const msgArgs = objectToMap<unknown>({ count: 14.2 });
 		const actual = mf.format(msgArgs);
 
 		expect(expectedMsg).to.equal(actual);
+	});
+
+	it('Simple plural test', () => {
+		const expectedMsgEq1 = 'Ai sters un fisier.\n';
+		const expectedMsgFew = 'Ai sters 3 fisiere.\n';
+		const expectedMsgOther = 'Ai sters 23 de fisiere.\n';
+
+		const partsEq1 = [
+			new PlainText('Ai sters un fisier.\n'),
+		];
+		const partsFew = [
+			new PlainText('Ai sters '),
+			new Placeholder('count', 'number', objectToMap<string>({})),
+			new PlainText(' fisiere.\n')
+		];
+		const partsOther = [
+			new PlainText('Ai sters '),
+			new Placeholder('count', 'number', objectToMap<string>({})),
+			new PlainText(' de fisiere.\n')
+		];
+
+		const localeRo = 'ro';
+		const mfEq1 = new SimpleMessage('', localeRo, partsEq1);
+		const mfEqFew = new SimpleMessage('', localeRo, partsFew);
+		const mfOther = new SimpleMessage('', localeRo, partsOther);
+
+		const switches = [
+			new Switch('count', 'plural')
+		];
+		const messages = new Map<ICase[], ISimpleMessage>();
+		messages.set([1], mfEq1);
+		messages.set(['few'], mfEqFew);
+		messages.set(['other'], mfOther);
+
+		const mf = new SelectorMessage('id', localeRo, switches, messages);
+
+		const msgArgs = objectToMap<unknown>({ count: 1 });
+		expect(expectedMsgEq1).to.equal(mf.format(msgArgs));
+		msgArgs.set('count', 3);
+		expect(expectedMsgFew).to.equal(mf.format(msgArgs));
+		msgArgs.set('count', 23);
+		expect(expectedMsgOther).to.equal(mf.format(msgArgs));
+	});
+
+	it('Simple gender test', () => {
+		const expectedMsgF = 'You\'ve been invited to her party.\n';
+		const expectedMsgM = 'You\'ve been invited to his party.\n';
+		const expectedMsgO = 'You\'ve been invited to their party.\n';
+
+		const mfEqF = new SimpleMessage('', locale, [new PlainText(expectedMsgF)]);
+		const mfEqM = new SimpleMessage('', locale, [new PlainText(expectedMsgM)]);
+		const mfEqO = new SimpleMessage('', locale, [new PlainText(expectedMsgO)]);
+
+		const switches = [
+			new Switch('host_gender', 'gender')
+		];
+		const messages = new Map<ICase[], ISimpleMessage>();
+		messages.set(['female'], mfEqF);
+		messages.set(['male'], mfEqM);
+		messages.set(['other'], mfEqO);
+
+		const mf = new SelectorMessage('id', locale, switches, messages);
+
+		const msgArgsEqF = objectToMap<unknown>({ host_gender: 'female' });
+		const msgArgsEqM = objectToMap<unknown>({ host_gender: 'male' });
+		const msgArgsEqO = objectToMap<unknown>({ host_gender: 'we_do_not_know' });
+
+		expect(expectedMsgF).to.equal(mf.format(msgArgsEqF));
+		expect(expectedMsgM).to.equal(mf.format(msgArgsEqM));
+		expect(expectedMsgO).to.equal(mf.format(msgArgsEqO));
+	});
+
+	it('Double plural test', () => {
+		const switches = [
+			new Switch('monster-count', 'plural'),
+			new Switch('dungeon-count', 'plural'),
+		];
+		var s0 = new SimpleMessage('', locale, [
+			new PlainText('You have killed no monsters.')]);
+		var s1 = new SimpleMessage('', locale, [
+			new PlainText('You have killed one monster in one dungeon.')]);
+		var s2 = new SimpleMessage('', locale, [
+			new PlainText('You have killed '),
+			new Placeholder('monster-count', 'number', new Map<string, string>([])),
+			new PlainText(' monsters in one dungeon.')
+		]);
+		var s3 = new SimpleMessage('', locale, [
+			new PlainText('You have killed '),
+			new Placeholder('monster-count', 'number', new Map<string, string>([])),
+			new PlainText(' monsters in '),
+			new Placeholder('dungeon-count', 'number', new Map<string, string>([])),
+			new PlainText(' dungeons.'),
+		]);
+
+		const messages = new Map<ICase[], ISimpleMessage>([
+			[[      0, 'other'], s0],
+			[[      1, 'other'], s1],
+			[['other',       1], s2],
+			[['other', 'other'], s3]
+		]);
+		const mf = new SelectorMessage('id', locale, switches, messages);
+
+		const args = objectToMap<unknown>();
+
+		args.set('monster-count', 0);
+		args.set('dungeon-count', 0);
+		expect('You have killed no monsters.').to.equal(mf.format(args));
+		args.set('monster-count', 1);
+		args.set('dungeon-count', 0);
+		expect('You have killed one monster in one dungeon.').to.equal(mf.format(args));
+		args.set('monster-count', 5);
+		args.set('dungeon-count', 1);
+		expect('You have killed 5 monsters in one dungeon.').to.equal(mf.format(args));
+		args.set('monster-count', 8);
+		args.set('dungeon-count', 2);
+		expect('You have killed 8 monsters in 2 dungeons.').to.equal(mf.format(args));
 	});
 });
