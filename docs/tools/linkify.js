3c96a99e1d2863deaec9ed9e14e3232531b3329d
Addison Phillips <addison@unicode.org>
2024-04-10 15:10:56 -0700

    Prettier

--- docs/tools/linkify.js
+++ docs/tools/linkify.js
@@ -3,63 +3,63 @@
 // this has been tested on the tr35-messageformat.html file
 // but not implemented in LDML45
 
-    const terms = new Array();
-    var missing = new Set();
-    
-    function findTerms() {
-       document.querySelectorAll('dfn').forEach(generateAnchor);
-       // console.log(terms.length);
-   }
-    
-    function generateAnchor(item, index) {
-        // console.log(index + ": " + item.textContent);
-        let t = generateId(item.textContent);
-        terms.push(t);
-        item.setAttribute('id', t);
-    }
-    
-    function linkify() {
-        const links = document.querySelectorAll('em');
-        links.forEach(checkLink);
-        // debugging
-        // console.log(terms.length);
-        // console.log(missing.length);
-        // console.log(missing);
-    }
-    
-    function checkLink(item) {
-        var t = generateId(item.textContent);
-        if (terms.includes(t)) {
-            link(item, t);
-        } else {
-            missing.add(t);
-        }
-    }
-    
-    function link(item, target) {
-        var a = document.createElement('a');
-        a.appendChild(document.createTextNode(item.textContent));
-        a.href = '#' + target;
+const terms = new Array();
+var missing = new Set();
 
-        var el = item.lastElementChild;
-        if (el) {
-           el.innerHTML = '';
-           el.appendChild(a);
-        } else {
-           item.innerHTML = '';
-           item.appendChild(a);
-        }
-    }
-    
-    function generateId(term) {
-        var retval = term.toLowerCase();
-        
-        if (retval.endsWith('rategies')) {
-            // found in the bidi isolation strategies
-            retval = retval.substring(0, term.length - 3) + 'y';
-        } else if (retval.endsWith('s') && retval !== 'status') {
-            // regular English plurals
-            retval = retval.substring(0, term.length - 1);
-        }
-        return retval.replaceAll(/ /g, '-');
-    }
+function findTerms() {
+  document.querySelectorAll("dfn").forEach(generateAnchor);
+  // console.log(terms.length);
+}
+
+function generateAnchor(item, index) {
+  // console.log(index + ": " + item.textContent);
+  let t = generateId(item.textContent);
+  terms.push(t);
+  item.setAttribute("id", t);
+}
+
+function linkify() {
+  const links = document.querySelectorAll("em");
+  links.forEach(checkLink);
+  // debugging
+  // console.log(terms.length);
+  // console.log(missing.length);
+  // console.log(missing);
+}
+
+function checkLink(item) {
+  var t = generateId(item.textContent);
+  if (terms.includes(t)) {
+    link(item, t);
+  } else {
+    missing.add(t);
+  }
+}
+
+function link(item, target) {
+  var a = document.createElement("a");
+  a.appendChild(document.createTextNode(item.textContent));
+  a.href = "#" + target;
+
+  var el = item.lastElementChild;
+  if (el) {
+    el.innerHTML = "";
+    el.appendChild(a);
+  } else {
+    item.innerHTML = "";
+    item.appendChild(a);
+  }
+}
+
+function generateId(term) {
+  var retval = term.toLowerCase();
+
+  if (retval.endsWith("rategies")) {
+    // found in the bidi isolation strategies
+    retval = retval.substring(0, term.length - 3) + "y";
+  } else if (retval.endsWith("s") && retval !== "status") {
+    // regular English plurals
+    retval = retval.substring(0, term.length - 1);
+  }
+  return retval.replaceAll(/ /g, "-");
+}
